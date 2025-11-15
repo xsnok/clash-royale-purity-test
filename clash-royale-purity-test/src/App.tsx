dcb2a268a4ee7b9c6c8796c426b3275cba840ab4
xsnok <chocowaco152@gmail.com>
2025-11-14 21:17:33 -0800

    Create starter page and form

--- clash-royale-purity-test/src/App.tsx
+++ clash-royale-purity-test/src/App.tsx
@@ -1,6 +1,12 @@
+import './App.css'
+import QuestionForm from "./components/QuestionForm"
+import { BrowserRouter } from "react-router-dom"
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <QuestionForm />
+    </BrowserRouter>
   )
 }
 
